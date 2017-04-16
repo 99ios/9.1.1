7932a96b7af2e1dbcf72d116e77855da37d314d9
test <422538722@qq.com>
2017-04-16 14:57:50 +0800

    9.1.1
    使用代码创建控制器

---
+++ 9.1.1 使用代码创建控制器/9.1.1 使用代码创建控制器/MYViewController.h
@@ -0,0 +1,13 @@
+//
+//  MYViewController.h
+//  9.1.1 使用代码创建控制器
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYViewController : UIViewController
+
+@end
---
+++ 9.1.1 使用代码创建控制器/9.1.1 使用代码创建控制器/MYViewController.m
@@ -0,0 +1,37 @@
+//
+//  MYViewController.m
+//  9.1.1 使用代码创建控制器
+//
+//  Created by 李维佳 on 2017/4/4.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import "MYViewController.h"
+
+@interface MYViewController ()
+
+@end
+
+@implementation MYViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
